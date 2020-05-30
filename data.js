2986e86e2063e2ff3ec3ee1adf69023c6627118d
JeremAIh <esiebomaj@gmail.com>
2020-05-30 20:02:18 +0100

    First commit Initialized and created all files and folders needed for this project

---
+++ data.js
@@ -0,0 +1,15 @@
+// It should be noted that this is just dummy data for now
+
+halls=[
+{
+	name:'Senate Building',
+	location:{longitude:21.4, latitude:45.32},
+	img_url='hall images/senate.jpg',
+
+},
+{
+	name:'Access Bank',
+	location:{longitude:21.4, latitude:45.32},
+	img_url='hall images/access.jpg',
+}
+]
