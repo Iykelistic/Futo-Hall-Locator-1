8f35178bbcffb456753335fcd29248977d1a3779
JeremAIh <esiebomaj@gmail.com>
2020-08-17 10:01:27 +0100

    worked on dark mode feature

--- data.js
+++ data.js
@@ -4,15 +4,91 @@ let halls = [
   {
     name: "Senate Building",
     coordinates: { latitude: 5.392804, longitude: 6.986247 },
-    img_url: "hall images/senate.jpg",
+    imgUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipNZCENYHB2FSHLu2xiuAw5zmZ2xkkwV7IOn2LTt=w426-h240-k-no",
     phoneNumber: 0987654098,
     addressLines: ["qwer gfd", "gfdsaz trew"],
+    info: "School Main admin building",
   },
   {
     name: "Access Bank",
     coordinates: { latitude: 5.3914571, longitude: 6.9838818 },
-    img_url: "hall images/access.jpg",
+    imgUrl:
+      "https://cdn.bmy.guide/images/688x350/60b86cfb57b2b3aa76ca19de184608f6_original.jpg",
     phoneNumber: 0987654098,
     addressLines: ["qwer gfd", "gfdsaz trew"],
+    info: "Bank",
+  },
+  {
+    name: "FUTO main gate",
+    coordinates: { latitude: 5.399956, longitude: 6.984115 },
+    imgUrl:
+      "https://2.bp.blogspot.com/-azgrMx_hYA0/Wh5gfdSgayI/AAAAAAAAAp0/gjrZsgTXWBIINGoOsoHW8epvXIHrLX7SgCLcBGAs/w1200-h630-p-k-no-nu/futo.jpg",
+    phoneNumber: 0987654098,
+    addressLines: ["qwer gfd", "gfdsaz trew"],
+    info: "University gate",
+  },
+  {
+    name: "NDDC Hostel Hall G",
+    coordinates: { latitude: 5.3930351, longitude: 7.0084377 },
+    imgUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipP6o-0-EKITnUpT0rcTBYdPrNWh5Wq-tlH10V5n=w426-h240-k-no",
+    phoneNumber: 0987654098,
+    addressLines: ["qwer gfd", "gfdsaz trew"],
+    info: "student hostel",
+  },
+  {
+    name: "NDDC Hostel ",
+    coordinates: { latitude: 5.3896221, longitude: 7.0067427 },
+    imgUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipNcoCh6U8de2cj-IBiYh_0ZQSf3fV5LnwVUYcmH=w426-h240-k-no",
+    phoneNumber: 0987654098,
+    addressLines: ["qwer gfd", "gfdsaz trew"],
+    info: "student hostel",
+  },
+  {
+    name: "Lecture hall 2 ",
+    coordinates: { latitude: 5.379675, longitude: 6.9959857 },
+    imgUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipPctdzUop1j3dYQJr2Z2dI65M_b-hUKtwtHq9CP=w426-h240-k-no",
+    phoneNumber: 0987654098,
+    addressLines: ["qwer gfd", "gfdsaz trew"],
+    info: "Lecture hall",
+  },
+  {
+    name: "Old Registrar ",
+    coordinates: { latitude: 5.378789, longitude: 6.996991 },
+    imgUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipPctdzUop1j3dYQJr2Z2dI65M_b-hUKtwtHq9CP=w426-h240-k-no",
+    phoneNumber: 0987654098,
+    addressLines: ["qwer gfd", "gfdsaz trew"],
+    info: "Bussiness Area",
+  },
+  {
+    name: "Workshop 2",
+    coordinates: { latitude: 5.3812246, longitude: 6.995717 },
+    imgUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipPctdzUop1j3dYQJr2Z2dI65M_b-hUKtwtHq9CP=w426-h240-k-no",
+    phoneNumber: 0987654098,
+    addressLines: ["qwer gfd", "gfdsaz trew"],
+    info: "Practical/Lecture venue",
+  },
+  {
+    name: "SEET Complex",
+    coordinates: { latitude: 5.3837428, longitude: 6.9974676 },
+    imgUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipPctdzUop1j3dYQJr2Z2dI65M_b-hUKtwtHq9CP=w426-h240-k-no",
+    phoneNumber: 0987654098,
+    addressLines: ["qwer gfd", "gfdsaz trew"],
+    info: "Lecture halls for engineering students",
+  },
+  {
+    name: "FUTO ICT",
+    coordinates: { latitude: 5.384522, longitude: 6.994986 },
+    imgUrl:
+      "https://lh5.googleusercontent.com/p/AF1QipPRze-wFqZr3rOauMkC7T8gECzsHRZg3YRu_OGT=w426-h240-k-no",
+    phoneNumber: 0987654098,
+    addressLines: ["qwer gfd", "gfdsaz trew"],
+    info: "Lecture halls for engineering students",
   },
 ];
--- index.js
+++ index.js
@@ -1,12 +1,137 @@
 var map,
   markers = [],
-  owerri = { lat: 5.386226, lng: 6.995472 };
+  owerri = { lat: 5.384522, lng: 6.994986 };
 var infoWindow;
+
+let dark = [
+  { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [{ color: "#263c3f" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#6b9a76" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [{ color: "#38414e" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#212a37" }],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#9ca5b3" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [{ color: "#746855" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#1f2835" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#f3d19c" }],
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [{ color: "#2f3948" }],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [{ color: "#17263c" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#515c6d" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.stroke",
+    stylers: [{ color: "#17263c" }],
+  },
+];
+
+let threeDStyle = [
+  {
+    featureType: "administrative",
+    elementType: "all",
+    stylers: [{ visibility: "off" }],
+  },
+  {
+    featureType: "landscape.natural",
+    elementType: "all",
+    stylers: [{ visibility: "off" }],
+  },
+  {
+    featureType: "poi",
+    elementType: "all",
+    stylers: [{ visibility: "off" }],
+  },
+  {
+    featureType: "road",
+    elementType: "all",
+    stylers: [{ visibility: "off" }],
+  },
+  {
+    featureType: "transit",
+    elementType: "all",
+    stylers: [{ visibility: "off" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels",
+    stylers: [{ visibility: "off" }],
+  },
+];
+let mapOptions = {
+  panControl: true,
+  zoomControl: true,
+  mapTypeControl: true,
+  scaleControl: true,
+  streetViewControl: true,
+  overviewMapControl: true,
+  rotateControl: true,
+};
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: owerri,
-    zoom: 10,
+    zoom: 15,
+    styles: null,
+    ...mapOptions,
   });
 
   var marker = new google.maps.Marker({
@@ -26,17 +151,16 @@ function displayhalls(halls) {
     phone = hall.phoneNumber;
     address = hall.addressLines;
     name = hall.name;
+    info = hall.info;
     hallhtml += `
     <div class="hall">
         
         <div class="hall-info-container">
           <div class="hall-address">
-            <p><span>${name}</span>
-             
-            
+           <span>${name}</span>            
           </div>
           <div class="hall-phone-number">
-            <p>${phone}</p>
+            <p>${info}</p>
           
           </div>
         </div>
@@ -58,27 +182,33 @@ function showhallsMArkers(halls) {
         hall.coordinates.longitude
       ),
       name = hall.name,
-      address = hall.addressLines[0];
+      info = hall.info;
+    imgUrl = hall.imgUrl;
 
+    address = hall.addressLines[0];
     status = hall.openStatusText;
     phone = hall.phoneNumber;
 
     // console.log(latlng)
-    createMarker(latlng, name, address, index);
+    createMarker(latlng, name, info, imgUrl, index);
     bounds.extend(latlng);
   });
-  map.fitBounds(bounds);
+  map.fitBounds(bounds, 0);
 }
 
 function searchhalls() {
-  foundhalls = [];
-  var zipCode = document.getElementById("zip-code-input").value;
-  if (zipCode) {
+  console.log("search starting");
+  let foundhalls = [];
+  const queryString = document.getElementById("zip-code-input").value;
+  if (queryString) {
     halls.forEach(function (hall, index) {
-      post_code = hall.address.postalCode.substring(0, 5);
-      if (post_code == zipCode) {
+      const hallName = hall.name;
+      console.log(hallName, queryString);
+      if (hallName.toLowerCase().indexOf(queryString) !== -1) {
         foundhalls.push(hall);
+        console.log("found one");
       }
+      console.log(foundhalls);
     });
   } else {
     foundhalls = halls;
@@ -108,29 +238,42 @@ function setOnClickListener() {
   });
 }
 
-function createMarker(latlng, name, address, index) {
-  var html = `
-    <div class="info-window">
+function createMarker(latlng, name, info, imgUrl, index) {
+  let mode = document.querySelector(".mode");
+  var html =
+    mode.textContent === "Dark Mode "
+      ? `
+    <div class="info-window dark">
       <div class="info-name">
         ${name}
       </div>
-      <div class="info-status">
-       ${status}
-      </div>
-      <div class="info-address">
-      <div class="circle1">
-        <i class="fas fa-location-arrow fast"></i>
-      </div>
-      ${address}
+      <div class="info-img">
+        <img height=80px src=${imgUrl}>
       </div>
       <div class="info-phone">
       <div class="circle1">
-        <i class="fas fa-phone-volume fast"></i>
+        <i class="fas fa-info-circle fast"></i>
       </div>
-       ${phone}
+       ${info}
       </div>
     </div>
-  `;
+  `
+      : `
+  <div class="info-window ">
+    <div class="info-name">
+      ${name}
+    </div>
+    <div class="info-img">
+      <img height=80px src=${imgUrl}>
+    </div>
+    <div class="info-phone">
+    <div class="circle1">
+      <i class="fas fa-info-circle fast"></i>
+    </div>
+     ${info}
+    </div>
+  </div>
+`;
   var marker = new google.maps.Marker({
     map: map,
     position: latlng,
@@ -142,3 +285,34 @@ function createMarker(latlng, name, address, index) {
   });
   markers.push(marker);
 }
+
+let mode = document.querySelector(".mode");
+mode.addEventListener("click", (event) => {
+  if (mode.textContent === "Dark Mode ") {
+    toggleDarkModeStyles();
+    searchhalls();
+    map.setOptions({ styles: dark });
+    mode.innerHTML = `Light Mode <i class="fas fa-sun"></i>`;
+  } else {
+    toggleDarkModeStyles();
+    searchhalls();
+    map.setOptions({ styles: null });
+    mode.innerHTML = `Dark Mode <i class="fas fa-adjust"></i>`;
+  }
+});
+
+const toggleDarkModeStyles = () => {
+  const footer = document.querySelector(".footer");
+  const title = document.querySelector(".title");
+  const hallListContainer = document.querySelector(".hall-list-container");
+
+  if (mode.textContent === "Dark Mode ") {
+    footer.style.color = "white";
+    title.style.color = "whitesmoke";
+    hallListContainer.classList.add("hall-list-dark");
+  } else {
+    footer.style.color = "black";
+    title.style.color = "black";
+    hallListContainer.classList.remove("hall-list-dark");
+  }
+};
